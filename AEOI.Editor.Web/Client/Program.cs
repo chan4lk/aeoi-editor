e1f9f2e525ce2b96483da198b5157aa15d4f3ec3
Chandima Ranaweera <chan4lk@gmail.com>
2021-11-03 07:21:40 +0530

    Telerik blazor added

--- AEOI.Editor.Web/Client/Program.cs
+++ AEOI.Editor.Web/Client/Program.cs
@@ -8,4 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// register the Telerik services
+builder.Services.AddTelerikBlazor();
+
 await builder.Build().RunAsync();
