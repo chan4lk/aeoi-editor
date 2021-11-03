1131c6f21d4334c1d72f17b7405583f74cac29af
Chandima Ranaweera <chan4lk@gmail.com>
2021-11-03 09:56:00 +0530

    XML grid loading

--- AEOI.Editor.Web/Client/Shared/Components/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AEOI.Editor.Web.Client.Shared.Components
-{
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Team { get; set; }
-    }
-}
---
+++ AEOI.Editor.Web/Server/Controllers/DataController.cs
@@ -0,0 +1,35 @@
+﻿using AEOI.Editor.Web.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Xml.Serialization;
+
+namespace AEOI.Editor.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+
+        public IWebHostEnvironment HostingEnvironment { get; set; }
+
+        public DataController(IWebHostEnvironment hostingEnvironment)
+        {
+            HostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("[action]")]
+        public AEOIUKSubmissionFIReport GetData(string fileName)
+        {
+            var path = Path.Combine(HostingEnvironment.ContentRootPath, "Uploads", fileName);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(AEOIUKSubmissionFIReport));
+
+            StreamReader reader = new StreamReader(path);
+            var report = (AEOIUKSubmissionFIReport)serializer.Deserialize(reader);
+            reader.Close();
+
+            return report;
+        }
+    }
+}
